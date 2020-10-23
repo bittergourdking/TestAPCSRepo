96c1eab3fd2bbf3699976cf5703fd293ea220c5d
bittergourdking <jkang21@stuy.edu>
2020-10-22 23:32:31 -0400

    I tested this program and it works now!

--- Demo.java
+++ Demo.java
@@ -20,6 +20,18 @@ public class Demo {
     return answer;
   }
 
+  public static String arrayDeepToString(int[][] arr) {
+    if (arr.length == 0) {
+    return "{}";
+    }
+    String answer = "{";
+    for (int i = 0; i < arr.length - 1; i++) {
+      answer = answer + arrToString(arr[i]) + ", ";
+    }
+    answer = answer + "}";
+    return answer;
+  }
+
 
   public static void main(String[] args) {
     if (args.length > 0) {
@@ -28,3 +40,30 @@ public class Demo {
       printLoop(5);
   }
 }
+
+/****************************************************************************
+TEST CASES
+
+For arrToString
+public static void main(String[] args) {
+  int[]one = {1, 2, 3, 4, 5};
+  int[]two = {10, 100, 1000};
+  int[]three = {0, 0, 0, 0, 0};
+  int[]four = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int[]five = {0, 5, 10, 15, 20};
+  System.out.println(arrToString(one));
+  System.out.println(arrToString(two));
+  System.out.println(arrToString(three));
+  System.out.println(arrToString(four));
+  System.out.println(arrToString(five));
+}
+
+For arrayDeepToString
+public static void main(String[] args) {
+int[][]one = {{1, 2, 3}, {}, {8, 2, 3, 1}, {1}};
+int[][]two = {{10, 100, 1000}, {0, 0, 0, 0, 0}, {0, 5, 10, 15, 20}};
+System.out.println(arrayDeepToString(one));
+System.out.println(arrayDeepToString(two));
+}
+
+****************************************************************************/
