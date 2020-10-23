6a3df5c6f24a6fae2bab783a22f25453f4082535
bittergourdking <jkang21@stuy.edu>
2020-10-22 23:19:15 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -7,6 +7,20 @@ public class Demo {
       System.out.println();
     }
   }
+
+  public static String arrToString(int[] arr) {
+    if (arr.length == 0) {
+    return "{}";
+    }
+    String answer = "{";
+    for (int i = 0; i < arr.length - 1; i++) {
+      answer = answer + arr[i] + ", ";
+    }
+    answer = answer + arr[arr.length - 1] + "}";
+    return answer;
+  }
+
+
   public static void main(String[] args) {
     if (args.length > 0) {
       printLoop(Integer.parseInt(args[0]));
